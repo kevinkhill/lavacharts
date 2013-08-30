2f9c86990d3f443e59d783ad07394d5477c2c0b2
Kevin Hill <kevinkhill@gmail.com>
2013-08-30 08:35:33 -0700

    added yuicompressor and created processing script

--- compressor.sh
+++ compressor.sh
@@ -1,28 +1,57 @@
 #!/bin/bash
 
-yui()
-{
-    echo "> Processing $FILE"
-    FILENAME=$(basename "$1")
-    EXT="${FILENAME##*.}"
+DIR=`pwd`
+
+echo "=========================================="
+echo "== Compressing CSS"
+echo "=========================================="
+for FILE in `ls -A "$DIR/public/css/"`
+do
+    FILENAME=$(basename "$FILE")
     FN="${FILENAME%.*}"
+    
+    echo "Processing $FILE"
+        java -jar yuicompressor-2.4.8.jar --charset utf-8 --type css "./public/css/$FN.css" -o "./public/builds/css/$FN.min.css"
+    echo "                                   ...Done"
+done
 
-    java -jar yuicompressor.jar "$FN.$EXT" -o "$NM.min.$EXT" --charset utf-8
-}
-
-echo "========== Compressing CSS =========="
-cd public/css/
-for FILE in *.css
+echo "=========================================="
+echo "== Compressing JS"
+echo "=========================================="
+for FILE in `ls -A "$DIR/public/js/" | grep -v ".swf"`
 do
-    yui $FILE
+    FILENAME=$(basename "$FILE")
+    FN="${FILENAME%.*}"
+    
+    echo "Processing $FILE"
+        java -jar yuicompressor-2.4.8.jar --charset utf-8 --type js "./public/js/$FN.js" -o "./public/builds/js/$FN.min.js" 
+    echo "                                   ...Done"
 done
 
 
-echo "========== Compressing JS =========="
-cd ../js/
-for FILE in *.css
-do
-    yui $FILE
-done
 
-echo "========== Complete! =========="
+
+
+echo "=========================================="
+echo "== Concatenating CSS"
+echo "=========================================="
+echo "Creating ./public/master.min.css"
+for FILE in `ls -A "$DIR/public/builds/css/"`
+do
+    cat "./public/builds/css/$FILE" >> "./public/master.min.css"
+done
+echo "                                   ...Done"
+
+echo "=========================================="
+echo "== Concatenating JS"
+echo "=========================================="
+echo "Creating ./public/master.min.js"
+for FILE in `ls -A "$DIR/public/builds/js/"`
+do
+    cat "./public/builds/js/$FILE" >> "./public/master.min.js"
+done
+echo "                                   ...Done"
+
+echo "=========================================="
+echo "== Complete!"
+echo "=========================================="
