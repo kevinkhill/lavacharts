2747d25debbb10fe57a1397ee7890ae69c0b0ef3
Kevin Hill <kevinkhill@gmail.com>
2014-06-09 12:48:35 -0700

    just found out what php traits are... I think I like this :)

--- src/Khill/Lavacharts/Charts/ComboChart.php
+++ src/Khill/Lavacharts/Charts/ComboChart.php
@@ -20,6 +20,8 @@ use Khill\Lavacharts\Helpers\Helpers;
 
 class ComboChart extends Chart
 {
+    use \Khill\Lavacharts\Traits\AxisTitlesPosition;
+
     public function __construct($chartLabel)
     {
         parent::__construct($chartLabel);
@@ -42,7 +44,7 @@ class ComboChart extends Chart
      * out - Draw the axis titles outside the chart area.
      * none - Omit the axis titles.
      *
-     * @param string $position
+     * @param annotation $position
      * @return \ComboChart
      */
     public function annotations($annotation)
@@ -57,32 +59,7 @@ class ComboChart extends Chart
         return $this;
     }
 
-    /**
-     * Where to place the axis titles, compared to the chart area. Supported values:
-     * in - Draw the axis titles inside the the chart area.
-     * out - Draw the axis titles outside the chart area.
-     * none - Omit the axis titles.
-     *
-     * @param string $position
-     * @return \ComboChart
-     */
-    public function axisTitlesPosition($position)
-    {
-        $values = array(
-            'in',
-            'out',
-            'none'
-        );
-
-        if(in_array($position, $values))
-        {
-            $this->addOption(array('axisTitlesPosition' => $position));
-        } else {
-            $this->type_error(__FUNCTION__, 'string', 'with a value of '.Helpers::array_string($values));
-        }
-
-        return $this;
-    }
+    // axisTitlesPosition
 
     /**
      * The width of a group of bars, specified in either of these formats:
