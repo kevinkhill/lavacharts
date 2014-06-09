549cdce38e2254177c0a1a43b4966a69ec1085d9
Kevin Hill <kevinkhill@gmail.com>
2014-06-09 13:25:22 -0700

    holding off on traits, don't want to bump to php 5.4 just yet

--- src/Khill/Lavacharts/Charts/ComboChart.php
+++ src/Khill/Lavacharts/Charts/ComboChart.php
@@ -20,7 +20,7 @@ use Khill\Lavacharts\Helpers\Helpers;
 
 class ComboChart extends Chart
 {
-    use \Khill\Lavacharts\Traits\AxisTitlesPosition;
+    //use \Khill\Lavacharts\Traits\AxisTitlesPosition;
 
     public function __construct($chartLabel)
     {
@@ -59,7 +59,32 @@ class ComboChart extends Chart
         return $this;
     }
 
-    // axisTitlesPosition
+    /**
+     * Where to place the axis titles, compared to the chart area. Supported values:
+     * in   - Draw the axis titles inside the the chart area.
+     * out  - Draw the axis titles outside the chart area.
+     * none - Omit the axis titles.
+     *
+     * @param string $position
+     * @return Chart
+     */
+    public function axisTitlesPosition($position)
+    {
+        $values = array(
+            'in',
+            'out',
+            'none'
+        );
+
+        if(in_array($position, $values))
+        {
+            $this->addOption(array('axisTitlesPosition' => $position));
+        } else {
+            $this->type_error(__FUNCTION__, 'string', 'with a value of '.Helpers::array_string($values));
+        }
+
+        return $this;
+    }
 
     /**
      * The width of a group of bars, specified in either of these formats:
