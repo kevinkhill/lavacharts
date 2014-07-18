92d484b22678fa4764362e7aac88ed2edd1f0127
Kevin Hill <kevinkhill@gmail.com>
2014-07-17 19:50:57 -0700

    added some chart tests

--- tests/Charts/ChartTest.php
+++ tests/Charts/ChartTest.php
@@ -7,6 +7,18 @@ class ChartTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         parent::setUp();
+
+        $this->chart = new Chart('test'); 
+    }
+
+    public function testInstanceOfChart()
+    {
+    	$this->assertInstanceOf('Khill\Lavacharts\Charts\Chart', $this->chart);
+    }
+
+    public function testLabelAssignedViaConstructor()
+    {
+    	$this->assertEquals('test', $this->chart->label);
     }
 
 }
--- tests/Charts/LineChartTest.php
+++ tests/Charts/LineChartTest.php
@@ -7,6 +7,22 @@ class LineChartTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         parent::setUp();
+
+        $this->linechart = new LineChart('test');
     }
 
+    public function testInstanceOfLineChartWithType()
+    {
+    	$this->assertInstanceOf('Khill\Lavacharts\Charts\LineChart', $this->linechart);
+    }
+
+    public function testTypeLineChart()
+    {
+    	$this->assertEquals('LineChart', $this->linechart->type);
+    }
+
+    public function testLabelAssignedViaConstructor()
+    {
+    	$this->assertEquals('test', $this->linechart->label);
+    }
 }
