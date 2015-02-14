79b3817fdedbd8f7e23bba2a69b33be491f3b6b8
Kevin Hill <kevinkhill@gmail.com>
2015-02-13 16:54:00 -0800

    Updating links in docblocks

--- src/Khill/Lavacharts/Configs/Color.php
+++ src/Khill/Lavacharts/Configs/Color.php
@@ -12,8 +12,8 @@
  * @since      v2.1.0
  * @author     Kevin Hill <kevinkhill@gmail.com>
  * @copyright  (c) 2014, KHill Designs
- * @link       http://github.com/kevinkhill/Lavacharts GitHub Repository Page
- * @link       http://kevinkhill.github.io/Lavacharts  GitHub Project Page
+ * @link       http://github.com/kevinkhill/lavacharts GitHub Repository Page
+ * @link       http://lavacharts.com                   Official Docs Site
  * @license    http://opensource.org/licenses/MIT MIT
  */
 
--- src/Khill/Lavacharts/Configs/DataTable.php
+++ src/Khill/Lavacharts/Configs/DataTable.php
@@ -19,8 +19,8 @@
  * @subpackage Configs
  * @author     Kevin Hill <kevinkhill@gmail.com>
  * @copyright  (c) 2014, KHill Designs
- * @link       http://github.com/kevinkhill/Lavacharts GitHub Repository Page
- * @link       http://kevinkhill.github.io/Lavacharts  GitHub Project Page
+ * @link       http://github.com/kevinkhill/lavacharts GitHub Repository Page
+ * @link       http://lavacharts.com                   Official Docs Site
  * @license    http://opensource.org/licenses/MIT MIT
  */
 
