ee6dbf4d586e81165dd06757e2e01eca8dfc5dda
Kevin Hill <kevinkhill@gmail.com>
2015-03-14 08:42:01 -0700

    added vAxes method

--- src/Khill/Lavacharts/Charts/BarChart.php
+++ src/Khill/Lavacharts/Charts/BarChart.php
@@ -362,6 +362,34 @@ class BarChart extends Chart
     }
 
     /**
+     * Specifies properties for individual vertical axes
+     *
+     * If the chart has multiple vertical axes. Each child object is a vAxis object,
+     * and can contain all the properties supported by vAxis.
+     * These property values override any global settings for the same property.
+     *
+     * To specify a chart with multiple vertical axes, first define a new axis using
+     * series.targetAxisIndex, then configure the axis using vAxes.
+     *
+     * @param  array              $arr Array of VerticalAxis objects
+     * @throws InvalidConfigValue
+     *
+     * @return BarChart
+     */
+    public function vAxes($arr)
+    {
+        if (Utils::arrayValuesCheck($arr, 'class', 'VerticalAxis')) {
+            return $this->addOption(array(__FUNCTION__ => $arr));
+        } else {
+            throw $this->invalidConfigValue(
+                __FUNCTION__,
+                'array',
+                'of VerticalAxis Objects'
+            );
+        }
+    }
+
+    /**
      * An object with members to configure various vertical axis elements. To
      * specify properties of this property, create a new VerticalAxis object, set
      * the values then pass it to this function or to the constructor.
