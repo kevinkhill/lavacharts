fc81895190c7d39d93347154c54ca84f851201b5
Kevin Hill <kevinkhill@gmail.com>
2015-04-24 23:36:34 -0700

    removed lava.js test. It was pointless to copy-paste the src into the test and check it

--- tests/LavachartsTest.php
+++ tests/LavachartsTest.php
@@ -298,12 +298,7 @@ class LavachartsTest extends ProvidersTestCase
 
     public function testJsapiMethodWithCoreJsTracking()
     {
-        $javascript  = '<script type="text/javascript" src="//www.google.com/jsapi"></script>';
-        $javascript .= '<script type="text/javascript">';
-        $javascript .= 'function onResize(a,e){return window.onresize=function(){clearTimeout(e),e=setTimeout(a,100)},a}var lava=lava||{get:null,event:null,charts:{},registeredCharts:[]};lava.get=function(a){var e,r=Object.keys(lava.charts);return"string"!=typeof a?(console.error("[Lavacharts] The input for lava.get() must be a string."),!1):Array.isArray(r)?void r.some(function(r){return"undefined"!=typeof lava.charts[r][a]?(e=lava.charts[r][a].chart,!0):!1}):!1},lava.event=function(a,e,r){return r(a,e)},lava.register=function(a,e){this.registeredCharts.push(a+":"+e)},window.onload=function(){onResize(function(){for(var a=0;a<lava.registeredCharts.length;a++){var e=lava.registeredCharts[a].split(":");lava.charts[e[0]][e[1]].draw()}})};';
-        $javascript .= '</script>';
-
-        $this->assertEquals($javascript, $this->lava->jsapi());
+        $this->lava->jsapi();
 
         $this->assertTrue($this->lava->jsFactory->coreJsRendered());
     }
