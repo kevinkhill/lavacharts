662378285fdb8b9526e4b43d730abe22aabab4d9
Kevin Hill <kevinkhill@gmail.com>
2015-08-04 07:06:15 -0700

    Adding option setting methods

--- src/Configs/UIs/CategoryUI.php
+++ src/Configs/UIs/CategoryUI.php
@@ -2,17 +2,12 @@
 
 namespace Khill\Lavacharts\Configs\UIs;
 
-use \Khill\Lavacharts\Exceptions\InvalidConfigProperty;
+use \Khill\Lavacharts\Utils;
+use \Khill\Lavacharts\Configs\Options;
+use \Khill\Lavacharts\Exceptions\InvalidConfigValue;
 
 class CategoryUI extends UI
 {
-    /**
-     * Allowed options to set for the UI.
-     *
-     * @var \Khill\Lavacharts\Configs\Options
-     */
-    private $options;
-
     /**
      * Default options available.
      *
@@ -32,6 +27,142 @@ class CategoryUI extends UI
         $options = new Options($this->defaults);
         $options->extend($this->extDefaults);
 
-        parent::__construct($config, $options);
+        parent::__construct($options, $config);
     }
+
+    /**
+     * The caption to display inside the value picker widget when no item is selected.
+     *
+     * @param  string $caption
+     * @throws \Khill\Lavacharts\Exceptions\InvalidConfigValue
+     * @return self
+     */
+    public function caption($caption)
+    {
+        if (Utils::nonEmptyString($caption) === false) {
+            throw new InvalidConfigValue(
+                __FUNCTION__,
+                'string'
+            );
+        }
+
+        return $this->setOption(__FUNCTION__, $caption);
+    }
+
+    /**
+     * Whether the values to choose from should be sorted.
+     *
+     * @param  boolean $sortValues
+     * @throws \Khill\Lavacharts\Exceptions\InvalidConfigValue
+     * @return self
+     */
+    public function sortValues($sortValues)
+    {
+        if (is_bool($sortValues) === false) {
+            throw new InvalidConfigValue(
+                __FUNCTION__,
+                'boolean'
+            );
+        }
+
+        return $this->setOption(__FUNCTION__, $sortValues);
+    }
+
+    /**
+     * How to display selected values, when multiple selection is allowed.
+     *
+     * Possible values are:
+     *  - 'aside': selected values will display in a single text line next to the value picker widget,
+     *  - 'below': selected values will display in a single text line below the widget,
+     *  - 'belowWrapping': similar to below, but entries that cannot fit in the picker will wrap to a new line,
+     *  - 'belowStacked': selected values will be displayed in a column below the widget.
+     *
+     * @param  string $selectedValuesLayout
+     * @throws \Khill\Lavacharts\Exceptions\InvalidConfigValue
+     * @return self
+     */
+    public function selectedValuesLayout($selectedValuesLayout)
+    {
+        $values = [
+            'aside',
+            'below',
+            'belowWrapping',
+            'belowStacked'
+        ];
+
+        if (Utils::nonEmptyStringInArray($selectedValuesLayout, $values) === false) {
+            throw new InvalidConfigValue(
+                __FUNCTION__,
+                'string',
+                ' whose accepted values are '.Utils::arrayToPipedString($values)
+            );
+        }
+
+        return $this->setOption(__FUNCTION__, $selectedValuesLayout);
+    }
+
+    /**
+     * Whether the user is allowed not to choose any value.
+     *
+     * If false the user must choose at least one value from the available ones.
+     * During control initialization, if the option is set to false and no selectedValues state is given,
+     * the first value from the available ones is automatically selected.
+     *
+     * @param  boolean $allowNone
+     * @throws \Khill\Lavacharts\Exceptions\InvalidConfigValue
+     * @return self
+     */
+    public function allowNone($allowNone)
+    {
+        if (is_bool($allowNone) === false) {
+            throw new InvalidConfigValue(
+                __FUNCTION__,
+                'boolean'
+            );
+        }
+
+        return $this->setOption(__FUNCTION__, $allowNone);
+    }
+
+    /**
+     * Whether multiple values can be selected, rather than just one.
+     *
+     * @param  boolean $allowMultiple
+     * @throws \Khill\Lavacharts\Exceptions\InvalidConfigValue
+     * @return self
+     */
+    public function allowMultiple($allowMultiple)
+    {
+        if (is_bool($allowMultiple) === false) {
+            throw new InvalidConfigValue(
+                __FUNCTION__,
+                'boolean'
+            );
+        }
+
+        return $this->setOption(__FUNCTION__, $allowMultiple);
+    }
+
+    /**
+     * Allow typing in a text field for filtering.
+     *
+     * Whether the user is allowed to type in a text field to narrow down
+     * the list of possible choices (via an autocompleter), or not.
+     *
+     * @param  boolean $allowTyping
+     * @throws \Khill\Lavacharts\Exceptions\InvalidConfigValue
+     * @return self
+     */
+    public function allowTyping($allowTyping)
+    {
+        if (is_bool($allowTyping) === false) {
+            throw new InvalidConfigValue(
+                __FUNCTION__,
+                'boolean'
+            );
+        }
+
+        return $this->setOption(__FUNCTION__, $allowTyping);
+    }
+
 }
--- src/Configs/UIs/ChartRangeUI.php
+++ src/Configs/UIs/ChartRangeUI.php
@@ -2,7 +2,9 @@
 
 namespace Khill\Lavacharts\Configs\UIs;
 
-use \Khill\Lavacharts\Exceptions\InvalidConfigProperty;
+use \Khill\Lavacharts\Utils;
+use \Khill\Lavacharts\Configs\Options;
+use \Khill\Lavacharts\Exceptions\InvalidConfigValue;
 
 class ChartRangeUI extends UI
 {
@@ -34,11 +36,35 @@ class ChartRangeUI extends UI
     }
 
     /**
+     * The type of the chart drawn inside the control.
+     * Accepted values are:
+     * - 'AreaChart'
+     * - 'LineChart'
+     * - 'ComboChart'
+     * - 'ScatterChart'
      *
+     * @param  string $chartType
+     * @throws \Khill\Lavacharts\Exceptions\InvalidConfigValue
+     * @return self
      */
-    public function chartType()
+    public function chartType($chartType)
     {
+        $values = [
+            'AreaChart',
+            'LineChart',
+            'ComboChart',
+            'ScatterChart'
+        ];
 
+        if (Utils::nonEmptyStringInArray($chartType, $values) === false) {
+            throw new InvalidConfigValue(
+                __FUNCTION__,
+                'string',
+                ' whose accepted values are '.Utils::arrayToPipedString($values)
+            );
+        }
+
+        return $this->setOption(__FUNCTION__, $chartType);
     }
 
     /**
@@ -46,7 +72,7 @@ class ChartRangeUI extends UI
      */
     public function chartOptions()
     {
-
+        //TODO: Decide on implementation
     }
 
     /**
@@ -54,26 +80,49 @@ class ChartRangeUI extends UI
      */
     public function chartView()
     {
-
+        //TODO: Decide on implementation
     }
 
     /**
+     * The minimum selectable range size (range.end - range.start), specified in data value units.
      *
+     * For a numeric axis, it is a number (not necessarily an integer).
+     * For a date, datetime or timeofday axis, it is an integer that specifies the difference in milliseconds.
+     *
+     * @param  int|float $minRangeSize Data value difference interpreted as 1 pixel
+     * @throws \Khill\Lavacharts\Exceptions\InvalidConfigValue
+     * @return self
      */
-    public function minRangeSize()
+    public function minRangeSize($minRangeSize)
     {
+        if (is_numeric($minRangeSize) === false) {
+            throw new InvalidConfigValue(
+                __FUNCTION__,
+                'int|float'
+            );
+        }
 
+        return $this->setOption(__FUNCTION__, $minRangeSize);
     }
 
     /**
+     * If true, range thumbs are snapped to the nearest data points.
      *
+     * In this case, the end points of the range returned by getState() are necessarily values in the data table.
+     *
+     * @param  boolean $snapToData
+     * @throws \Khill\Lavacharts\Exceptions\InvalidConfigValue
+     * @return self
      */
-    public function snapToData()
+    public function snapToData($snapToData)
     {
+        if (is_bool($snapToData) === false) {
+            throw new InvalidConfigValue(
+                __FUNCTION__,
+                'boolean'
+            );
+        }
 
+        return $this->setOption(__FUNCTION__, $snapToData);
     }
-
-    /**
-     *
-     */
 }
