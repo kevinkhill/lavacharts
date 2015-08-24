d2fddb63a16880687af61d34d0b71a98cff289d8
Kevin Hill <kevinkhill@gmail.com>
2015-08-24 07:20:05 -0700

    cleaning up karma and gulp

--- configs/karma.conf.js
+++ configs/karma.conf.js
@@ -1,74 +1,101 @@
-// Karma configuration
-// Generated on Mon Apr 27 2015 15:40:10 GMT-0700 (PDT)
-
 module.exports = function(config) {
-  config.set({
+    config.set({
+        basePath: '../javascript',
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '../javascript',
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: [
+            'jasmine',
+            'detectBrowsers'
+        ],
 
+        // list of files / patterns to load in the browser
+        files: [
+            'lava.js',
+            'lava.spec.js'
+        ],
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+        // list of files to exclude
+        exclude: [
+        ],
 
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+        },
 
-    // list of files / patterns to load in the browser
-    files: [
-      'lava.js',
-      'lava.spec.js'
-    ],
+        //CI Mode
+        singleRun: true,
 
+        plugins: [
+            'karma-jasmine',
+            'karma-detect-browsers',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-phantomjs-launcher'
+        ],
 
-    // list of files to exclude
-    exclude: [
-    ],
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: [
+            'progress'
+        ],
 
+        // web server port
+        port: 9876,
 
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
-    plugins: [
-        'karma-jasmine',
-        'karma-chrome-launcher',
-        'karam-firefox-launcher',
-        'karma-phantomjs-launcher'
-    ],
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
 
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+        browsers: ['Chrome'],
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+        // detectBrowsers Configuration
+        detectBrowsers: {
+            // enable/disable, default is true
+            enabled: true,
 
+            // enable/disable phantomjs support, default is true
+            usePhantomJS: true,
 
-    // web server port
-    port: 9876,
+            // post processing of browsers list
+            // here you can edit the list of browsers used by karma
+            postDetection: function(availableBrowser) {
+                /* Karma configuration with custom launchers
+                 customLaunchers: {
+                 IE9: {
+                 base: 'IE',
+                 'x-ua-compatible': 'IE=EmulateIE9'
+                 }
+                 }
+                 */
 
+                //Add IE Emulation
+                var result = availableBrowser;
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+                if (availableBrowser.indexOf('IE')>-1) {
+                    result.push('IE9');
+                }
 
+                //Remove PhantomJS if another browser has been detected
+                if (availableBrowser.length > 1 && availableBrowser.indexOf('PhantomJS')>-1) {
+                    var i = result.indexOf('PhantomJS');
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+                    if (i !== -1) {
+                        result.splice(i, 1);
+                    }
+                }
 
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-  });
+                return result;
+            }
+        }
+    });
 };
--- gulpfile.js
+++ gulpfile.js
@@ -7,56 +7,48 @@ var gulp = require('gulp'),
    karma = require('karma').server;
 
 
-gulp.task('serve', function (done) {
-  sh('php ../../../artisan serve');
-});
-
-gulp.task('phpunit', function (done) {
-  sh('./vendor/bin/phpunit -c configs/phpunit.xml');
-});
-
-gulp.task('sami', function (done) {
-  sh('./vendor/bin/sami.php update configs/sami.cfg.php');
-});
-
 gulp.task('karma', function (done) {
-  var conf = {
-    configFile: __dirname + '/configs/karma.conf.js'
-  };
+    var conf = {
+      configFile: __dirname + '/configs/karma.conf.js'
+    };
 
-  if (typeof argv.browsers !== 'undefined') {
-    conf.browsers = argv.browsers;
-  }
+    if (argv.dev) {
+      conf.singleRun = false;
+    }
 
-  if (argv.dev) {
-    conf.singleRun = false;
-  }
-
-  karma.start(conf, done);
+    karma.start(conf, done);
 });
 
-gulp.task('check', function (done) {
-  sh('./vendor/bin/phpcs -n --standard=PSR2 ./src ./tests');
+gulp.task('php:test', function (done) {
+    sh('./vendor/bin/phpunit -c configs/phpunit.xml');
 });
 
-gulp.task('fix', function (done) {
-  sh('./vendor/bin/phpcbf -n --standard=PSR2 ./src ./tests');
+gulp.task('php:doc', function (done) {
+    sh('./vendor/bin/sami.php update configs/sami.cfg.php');
 });
 
-gulp.task('lint', function (done) {
-  gulp.src('./javascript/lava.js')
-      .pipe(jshint());
+gulp.task('php:cs', function (done) {
+    sh('./vendor/bin/phpcs -n --standard=PSR2 ./src ./tests');
+});
+
+gulp.task('php:fix', function (done) {
+    sh('./vendor/bin/phpcbf -n --standard=PSR2 ./src ./tests');
+});
+
+gulp.task('js:lint', function (done) {
+    gulp.src('./javascript/lava.js')
+        .pipe(jshint());
 });
 
 gulp.task('bump', function (done) { //-v=1.2.3
-  var version = argv.v;
-  var minorVersion = version.slice(0, -2);
+    var version = argv.v;
+    var minorVersion = version.slice(0, -2);
 
-  gulp.src('./package.json')
-      .pipe(bump({version:argv.v}))
-      .pipe(gulp.dest('./'));
+    gulp.src('./package.json')
+        .pipe(bump({version:argv.v}))
+        .pipe(gulp.dest('./'));
 
-  gulp.src(['./README.md', './.travis.yml'])
-      .pipe(replace(/("|=|\/|-)[0-9]+\.[0-9]+/g, '$1'+minorVersion))
-      .pipe(gulp.dest('./'));
+    gulp.src(['./README.md', './.travis.yml'])
+        .pipe(replace(/("|=|\/|-)[0-9]+\.[0-9]+/g, '$1'+minorVersion))
+        .pipe(gulp.dest('./'));
 });
