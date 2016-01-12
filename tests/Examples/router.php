61680ea7985af07e5368a7e0986d8f58f8316844
Kevin Hill <kevinkhill@gmail.com>
2016-01-12 10:31:42 -0800

    added date.timezone

---
+++ tests/Examples/router.php
@@ -0,0 +1,14 @@
+<?php
+
+require('../../vendor/autoload.php');
+
+if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
+    return false;    // serve the requested resource as-is.
+} else {
+    $chart = trim($_SERVER["REQUEST_URI"], '/');
+
+    $lava = new \Khill\Lavacharts\Lavacharts;
+
+    require_once(__DIR__ . '\\' . $chart . '.php');
+}
+
