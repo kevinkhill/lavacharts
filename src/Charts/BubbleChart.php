e3684a6f78e7147843f0da9867f1836c52cee8e5
Kevin Hill <kevinkhill@gmail.com>
2016-02-02 13:50:44 -0800

    adding bubblechart

---
+++ src/Charts/BubbleChart.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Khill\Lavacharts\Charts;
+
+/**
+ * GaugeChart Class
+ *
+ *
+ * A bubble chart is used to visualize a data set with two to four dimensions.
+ * The first two dimensions are visualized as coordinates, the third as color
+ * and the fourth as size.
+ *
+ *
+ * @package    Khill\Lavacharts
+ * @subpackage Charts
+ * @since      3.0.1
+ * @author     Kevin Hill <kevinkhill@gmail.com>
+ * @copyright  (c) 2015, KHill Designs
+ * @link       http://github.com/kevinkhill/lavacharts GitHub Repository Page
+ * @link       http://lavacharts.com                   Official Docs Site
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+class BubbleChart extends Chart
+{
+    /**
+     * Javascript chart type.
+     *
+     * @var string
+     */
+    const TYPE = 'BubbleChart';
+
+    /**
+     * Javascript chart version.
+     *
+     * @var string
+     */
+    const VERSION = '1';
+
+    /**
+     * Javascript chart package.
+     *
+     * @var string
+     */
+    const VIZ_PACKAGE = 'corechart';
+
+    /**
+     * Google's visualization class name.
+     *
+     * @var string
+     */
+    const VIZ_CLASS = 'google.visualization.BubbleChart';
+}
