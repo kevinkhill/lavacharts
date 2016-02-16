20f7afe12b1cf554fe50ba4bb26debfef852e16c
Kevin Hill <kevinkhill@gmail.com>
2016-02-16 14:00:17 -0800

    Refactoring lava.js

--- javascript/lava.js
+++ javascript/lava.js
@@ -1,13 +1,7 @@
-/**
- * jvent.min.js
- *
- * Author:   Guille Paz
- * Email:    guille87paz@gmail.com
- * Github:   https://github.com/pazguille/jvent
- * Homepage: http://pazguille.me
- * License:  MIT
- */
-!function(t){"use strict";function e(){}e.prototype.on=function(t,e){return this._collection=this._collection||{},this._collection[t]=this._collection[t]||[],this._collection[t].push(e),this},e.prototype.once=function(t,e){function i(){o.off(t,i),e.apply(this,arguments)}var o=this;return i.listener=e,this.on(t,i),this},e.prototype.off=function(t,e){var i=this._collection[t],o=0;if(void 0!==i)for(o;o<i.length;o+=1)if(i[o]===e||i[o].listener===e){i.splice(o,1);break}return 0===i.length&&this.removeAllListeners(t),this},e.prototype.removeAllListeners=function(t){return this._collection=this._collection||{},delete this._collection[t],this},e.prototype.listeners=function(t){return this._collection=this._collection||{},this._collection[t]},e.prototype.emit=function(){if(void 0===this._collection)return this;var t,e=[].slice.call(arguments,0),i=e.shift(),o=this._collection[i],n=0;if(o)for(o=o.slice(0),t=o.length,n;t>n;n+=1)o[n].apply(this,e);return this},"function"==typeof t.define&&void 0!==t.define.amd?t.define("Jvent",[],function(){return e}):"undefined"!=typeof module&&void 0!==module.exports?module.exports=e:t.Jvent=e}(this);
+"use strict";
+
+const EventEmitter = require('events');
+const util = require('util');
 
 /**
  * lava.js
@@ -17,234 +11,267 @@
  * Github:  https://github.com/kevinkhill/lavacharts
  * License: MIT
  */
-var lava = lava || {};
+var lava = function() {
+  EventEmitter.call(this);
+  this._ = require('underscore');
 
-(function() {
-  "use strict";
+  this._charts            = [];
+  this._dashboards        = [];
+  this._registeredCharts  = [];
+  this._readyCallback     = function(){};
+  this._renderDashboards  = null;
+  this.errors = require('./lava.errors.js')
+};
 
-  this.charts            = [];
-  this.dashboards        = [];
-  this.registeredCharts  = [];
-  this.readyCallback     = null;
-  this.renderDashboards  = null;
+util.inherits(lava, EventEmitter);
 
-  this.events = new Jvent();
-
-  //var registeredActions = [];
-
-  /**
-   * LavaChart object.
-   *
-   * @constructor
-   */
-  this.Chart = function() {
-    var self = this;
-    this.data    = null;
-    this.chart   = null;
-    this.options = null;
-    this.formats = [];
-    this.render  = function(){};
-    this.setData = function(){};
-    this.redraw  = function(){};
-    this.applyFormats = function (formatArr) {
-      for(var a=0; a < formatArr.length; a++) {
-        var formatJson = formatArr[a];
-        var formatter = new google.visualization[formatJson.type](formatJson.config);
-        formatter.format(self.data, formatJson.index);
-      }
-    };
-  };
-
-  /**
-   * Dashboard object.
-   *
-   * @constructor
-   */
-  this.Dashboard = function() {
-    this.render    = null;
-    this.data      = null;
-    this.bindings  = [];
-    this.dashboard = null;
-    this.callbacks = [];
-  };
-
-  this.Callback = function (label, func) {
-    this.label = label;
-    this.func  = func;
-  };
-
-  this.ready = function (callback) {
-    if (typeof callback !== 'function') {
-      throw new Error('[Lavacharts] ' + typeof callback + ' is not a valid callback.');
-    } else {
-      var callback = function(){};
-    }
-
-    lava.readyCallback = callback;
-  };
-
-  /**
-   * Event wrapper for chart events.
-   *
-   *
-   * Used internally when events are applied so the user event function has
-   * access to the chart within the event callback.
-   *
-   * @param {object} event
-   * @param {object} chart
-   * @param {function} callback
-   */
-  this.event = function (event, chart, callback) {
-    return callback(event, chart);
-  };
-
-  /**
-   * Registers a chart as being on screen, accessible to redraws.
-   */
-  this.registerChart = function(type, label) {
-    this.registeredCharts.push(type + ':' + label);
-  };
-
-  /**
-   * Loads a new DataTable into the chart and redraws.
-   *
-   *
-   * Used with an AJAX call to a PHP method returning DataTable->toJson(),
-   * a chart can be dynamically update in page, without reloads.
-   *
-   * @param {string} chartLabel
-   * @param {string} json
-   * @param {function} callback
-   */
-  this.loadData = function (chartLabel, json, callback) {
-    lava.getChart(chartLabel, function (chart, LavaChart) {
-      if (typeof json.data != 'undefined') {
-        LavaChart.setData(json.data);
-      } else {
-        LavaChart.setData(json);
-      }
-
-      if (typeof json.formats != 'undefined') {
-        LavaChart.applyFormats(json.formats);
-      }
-
-      LavaChart.redraw();
-
-      if (typeof callback == 'function') {
-        callback(LavaChart.chart);
-      }
-    });
-  };
-
-  this.getDashboard = function (label, callback) {
-    if (typeof lava.dashboards[label] === 'undefined') {
-      throw new Error('[Lavacharts] Dashboard "' + label + '" was not found.');
-    }
-
-    var lavaDashboard = lava.dashboards[label];
-
-    if (typeof callback !== 'function') {
-      throw new Error('[Lavacharts] ' + typeof callback + ' is not a valid callback.');
-    }
-
-    callback(lavaDashboard.dashboard, lavaDashboard);
-  };
-
-  /**
-   * Returns the GoogleChart and the LavaChart objects
-   *
-   *
-   * The GoogleChart object can be used to access any of the available methods such as
-   * getImageURI() or getChartLayoutInterface().
-   * See https://google-developers.appspot.com/chart/interactive/docs/gallery/linechart#methods
-   * for some examples relative to LineCharts.
-   *
-   * The LavaChart object holds all the user defined properties such as data, options, formats,
-   * the google chart object, and relative methods for internal use.
-   *
-   * Just to clarify:
-   *  - The first returned callback value is a property of the LavaChart.
-   *    It was add as a shortcut to avoid chart.chart to access google's methods of the chart.
-   *
-   *  - The second returned callback value is the LavaChart, which holds the GoogleChart and other
-   *    important information. It was added to not restrict the user to only getting the GoogleChart
-   *    returned, and as the second value because it is less useful / rarely accessed.
-   *
-   * @param  {string}   chartLabel
-   * @param  {function} callback
-   */
-  this.getChart = function (chartLabel, callback) {
-    if (typeof chartLabel != 'string') {
-      throw new Error('[Lavacharts] ' + typeof chartLabel + ' is not a valid chart label.');
-    }
-
-    if (typeof callback != 'function') {
-      throw new Error('[Lavacharts] ' + typeof callback + ' is not a valid callback.');
-    }
-
-    var chartTypes = Object.keys(lava.charts);
-    var LavaChart;
-
-    var search = chartTypes.some(function (type) {
-      if (typeof lava.charts[type][chartLabel] !== 'undefined') {
-        LavaChart = lava.charts[type][chartLabel];
-
-        return true;
-      } else {
-        return false;
-      }
-    });
-
-    if (search === false) {
-      throw new Error('[Lavacharts] Chart "' + chartLabel + '" was not found.');
-    }
-
-    callback(LavaChart.chart, LavaChart);
-  };
-
-  /**
-   * Redraws all of the registered charts on screen.
-   *
-   *
-   * This method is attached to the window resize event with a 300ms debounce
-   * to make the charts responsive to the browser resizing.
-   */
-  this.redrawCharts = function() {
-    var timer, delay = 300;
-
-    clearTimeout(timer);
-
-    timer = setTimeout(function() {
-      for(var c = 0; c < lava.registeredCharts.length; c++) {
-        var parts = lava.registeredCharts[c].split(':');
-
-        lava.charts[parts[0]][parts[1]].redraw();
-      }
-    }, delay);
-  };
-
-  this.run = function () {
-    var s = document.createElement('script');
-    s.type = 'text/javascript';
-    s.src = '//www.google.com/jsapi';
-    s.onload = s.onreadystatechange = function (event) {
-      event = event || window.event;
-
-      if (event.type === "load" || (/loaded|complete/.test(this.readyState))) {
-        this.onload = this.onreadystatechange = null;
-
-        lava.events.emit('jsapi:ready', window.google);
-      }
-    };
-
-    document.head.appendChild(s);
-  };
-
-}).apply(lava);
+module.exports = new lava();
 
 /**
- * Adding the resize event listener for redrawing charts.
+ * LavaChart object.
+ *
+ * @constructor
  */
-window.addEventListener("resize", window.lava.redrawCharts);
+lava.prototype.Chart = function (type, label) {
+  //var self = this;
+  this.type    = type;
+  this.label   = label;
+  this.data    = null;
+  this.chart   = null;
+  this.options = null;
+  this.formats = [];
+  this.element = null;
+  this.render  = function(){};
 
-lava.run();
+  this.setData = function (data) {
+    this.data = new google.visualization.DataTable(data, '0.6');
+  };
+
+  this.setElement = function (elemId) {
+    this.element = document.getElementById(elemId);
+
+    if (! this.element) {
+      throw this.errors.ELEMENT_ID_NOT_FOUND(elemId);
+    }
+  };
+
+  this.redraw = function() {
+    this.chart.draw(this.data, this.options);
+  };
+
+  this.applyFormats = function (formatArr) {
+    for(var a=0; a < formatArr.length; a++) {
+      var formatJson = formatArr[a];
+      var formatter = new google.visualization[formatJson.type](formatJson.config);
+
+      formatter.format(this.data, formatJson.index);
+    }
+  };
+};
+
+/**
+ * Dashboard object.
+ *
+ * @constructor
+ */
+lava.prototype.Dashboard = function() {
+  this.render    = null;
+  this.data      = null;
+  this.bindings  = [];
+  this.dashboard = null;
+};
+
+lava.prototype.DataTable = function (data) {
+  return new window.google.visualization.DataTable(data);
+};
+
+lava.prototype.initChartType = function (type) {
+  console.log(type);
+  if ( typeof this._charts[type] == "undefined" ) {
+    this._charts[type] = [];
+  }
+}
+
+lava.prototype.ready = function (callback) {
+  if (typeof callback !== 'function') {
+    throw this.errors.INVALID_CALLBACK(callback);
+  }
+
+  this._readyCallback = callback;
+};
+
+/**
+ * Event wrapper for chart events.
+ *
+ *
+ * Used internally when events are applied so the user event function has
+ * access to the chart within the event callback.
+ *
+ * @param {object} event
+ * @param {object} chart
+ * @param {function} callback
+ */
+lava.prototype.event = function (event, chart, callback) {
+  return callback(event, chart);
+};
+
+/**
+ * Registers a chart as being on screen, accessible to redraws.
+ */
+lava.prototype.registerChart = function(type, label) {
+  this._registeredCharts.push(type + ':' + label);
+};
+
+/**
+ * Loads a new DataTable into the chart and redraws.
+ *
+ *
+ * Used with an AJAX call to a PHP method returning DataTable->toJson(),
+ * a chart can be dynamically update in page, without reloads.
+ *
+ * @param {string} chartLabel
+ * @param {string} json
+ * @param {function} callback
+ */
+lava.prototype.loadData = function (chartLabel, json, callback) {
+  this.getChart(chartLabel, function (chart, LavaChart) {
+    if (typeof json.data != 'undefined') {
+      LavaChart.setData(json.data);
+    } else {
+      LavaChart.setData(json);
+    }
+
+    if (typeof json.formats != 'undefined') {
+      LavaChart.applyFormats(json.formats);
+    }
+
+    LavaChart.redraw();
+
+    if (typeof callback == 'function') {
+      callback(LavaChart.chart);
+    }
+  });
+};
+
+/**
+ * Retrieve a Dashboard from lava.js
+ *
+ * @param  {string}   label    Dashboard label.
+ * @param  {Function} callback Callback function
+ */
+lava.prototype.getDashboard = function (label, callback) {
+  if (typeof this._dashboards[label] === 'undefined') {
+    throw this.errors.DASHBOARD_NOT_FOUND(label);
+  }
+
+  var lavaDashboard = this._dashboards[label];
+
+  if (typeof callback !== 'function') {
+    throw this.errors.INVALID_CALLBACK(callback);
+  }
+
+  callback(lavaDashboard.dashboard, lavaDashboard);
+};
+
+/**
+ * Stores a chart within lava.js
+ *
+ * @param  Chart chart
+ */
+lava.prototype.storeChart = function (chart) {
+  this._charts[chart.type][chart.label] = chart;
+}
+
+/**
+ * Returns the GoogleChart and the LavaChart objects
+ *
+ *
+ * The GoogleChart object can be used to access any of the available methods such as
+ * getImageURI() or getChartLayoutInterface().
+ * See https://google-developers.appspot.com/chart/interactive/docs/gallery/linechart#methods
+ * for some examples relative to LineCharts.
+ *
+ * The LavaChart object holds all the user defined properties such as data, options, formats,
+ * the google chart object, and relative methods for internal use.
+ *
+ * Just to clarify:
+ *  - The first returned callback value is a property of the LavaChart.
+ *    It was add as a shortcut to avoid chart.chart to access google's methods of the chart.
+ *
+ *  - The second returned callback value is the LavaChart, which holds the GoogleChart and other
+ *    important information. It was added to not restrict the user to only getting the GoogleChart
+ *    returned, and as the second value because it is less useful / rarely accessed.
+ *
+ * @param  {string}   chartLabel
+ * @param  {function} callback
+ */
+lava.prototype.getChart = function (chartLabel, callback) {
+  if (typeof chartLabel != 'string') {
+    throw this.errors.INVALID_LABEL(chartLabel);
+  }
+
+  if (typeof callback != 'function') {
+    throw this.errors.INVALID_CALLBACK(callback);
+  }
+
+  var chartTypes = Object.keys(this._charts);
+  console.log(chartTypes);
+  var LavaChart;
+
+  var chartFound = this._.some(chartTypes, this._.bind(function (chartLabel, type) {
+    console.log(type);
+
+    if (typeof this._charts[type][chartLabel] !== 'undefinded') {
+      LavaChart = this._charts[type][chartLabel];
+
+      return true;
+    }
+
+    return false;
+  }, this, chartLabel));
+
+  if (chartFound === false) {
+    throw this.errors.CHART_NOT_FOUND(chartLabel);
+  } else {
+    callback(LavaChart.chart, LavaChart);
+  }
+};
+
+/**
+ * Redraws all of the registered charts on screen.
+ *
+ *
+ * This method is attached to the window resize event with a 300ms debounce
+ * to make the charts responsive to the browser resizing.
+ */
+lava.prototype.redrawCharts = function() {
+  var timer, delay = 300;
+
+  clearTimeout(timer);
+
+  timer = setTimeout(function() {
+    for(var c = 0; c < this._registeredCharts.length; c++) {
+      var parts = this._registeredCharts[c].split(':');
+
+      this._charts[parts[0]][parts[1]].redraw();
+    }
+  }.bind(this), delay);
+};
+
+lava.prototype.jsapi = function() {
+  var s = document.createElement('script');
+
+  s.type = 'text/javascript';
+  s.src = '//www.google.com/jsapi';
+  s.onload = s.onreadystatechange = function (event) {
+    event = event || window.event;
+
+    if (event.type === "load" || (/loaded|complete/.test(this.readyState))) {
+      this.onload = this.onreadystatechange = null;
+
+      this.emit('jsapi:ready', window.google);
+    }
+  }.bind(this);
+
+  document.head.appendChild(s);
+};
---
+++ javascript/lavacharts.js
@@ -0,0 +1,16 @@
+(function(){
+  "use strict"
+
+  this.lava = require('./lava.js');
+
+  /**
+   * Adding the resize event listener for redrawing charts.
+   */
+  this.addEventListener("resize", this.lava.redrawCharts);
+
+  /**
+   * Initialize the lava.js module by downloading Google's jsapi
+   */
+  this.lava.jsapi();
+
+}).apply(window);
