aece76cec42817fd25458f3557b07971c1159222
Kevin Hill <kevinkhill@gmail.com>
2016-04-09 10:39:35 -0700

    Adding example of datefilter on dashboard

--- tests/Examples/router.php
+++ tests/Examples/router.php
@@ -34,9 +34,9 @@ if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
             h1, h2 {font-family:Helvetica,Verdana,sans-serif;}
             #logo{text-align:center}
             #lavachart{width:99%}
-            .float{float:left;margin-left:20px;}
+            .float{float:left;padding:0 20px;margin-left:20px;}
             .grey{background-color:#f3f3f3;border:1px solid #666}
-            ul{list-style-type:none;margin:0;padding:0}li{font:200 16px/1.5 Helvetica,Verdana,sans-serif;border-bottom:1px solid #ccc}li:last-child{border:none}li a{text-decoration:none;color:#000;display:block;width:200px;-webkit-transition:font-size .2s ease,background-color .2s ease;-moz-transition:font-size .2s ease,background-color .2s ease;-o-transition:font-size .2s ease,background-color .2s ease;-ms-transition:font-size .2s ease,background-color .2s ease;transition:font-size .2s ease,background-color .2s ease}li a:hover{font-size:20px;background:#f6f6f6}
+            ul{list-style-type:none;margin:0;padding:0}li{font:200 16px/1.5 Helvetica,Verdana,sans-serif;border-bottom:1px solid #ccc}li:last-child{border:none}li a{text-decoration:none;color:#000;display:block;width:200px;-webkit-transition:font-size .2s ease,background-color .2s ease;-moz-transition:font-size .2s ease,background-color .2s ease;-o-transition:font-size .2s ease,background-color .2s ease;-ms-transition:font-size .2s ease,background-color .2s ease;transition:font-size .2s ease,background-color .2s ease}li a:hover{font-size:18px;background:#f6f6f6}
         </style>
     </head>
     <body>
@@ -99,6 +99,7 @@ if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
                     <li><a href="OneToMany">One to Many</a></li>
                     <li><a href="ManyToOne">Many to One</a></li>
                     <li><a href="ManyToMany">Many to Many</a></li>
+                    <li><a href="OneToOneDateFilter">One to One [DateFilter]</a></li>
                 </ul>
             </div>
 <?php
