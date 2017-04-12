91276af2aaca6be4048c7e1d73421465c72eaf52
Kevin Hill <kevinkhill@gmail.com>
2017-04-11 18:33:42 -0700

    Bringing the Symfony code up to date

--- src/Symfony/Bundle/Resources/docs/README.md
+++ src/Symfony/Bundle/Resources/docs/README.md
@@ -8,7 +8,7 @@ Open a command console, enter your project directory and execute the
 following command to download the latest stable version of this bundle:
 
 ```bash
-$ composer require khill/lavacharts "~3.0"
+$ composer require khill/lavacharts
 ```
 
 This command requires you to have Composer installed globally, as explained
--- src/Symfony/Bundle/Twig/LavachartsExtension.php
+++ src/Symfony/Bundle/Twig/LavachartsExtension.php
@@ -3,9 +3,17 @@
 namespace Khill\Lavacharts\Symfony\Bundle\Twig;
 
 use Khill\Lavacharts\Lavacharts;
+use Khill\Lavacharts\Charts\ChartFactory;
 
 class LavachartsExtension extends \Twig_Extension
 {
+    /**
+     * Twig Extension Name
+     *
+     * @var string
+     */
+    const NAME = 'lavacharts_twig_extension';
+
     /**
      * The Lavacharts object passed in from the service container.
      *
@@ -13,23 +21,31 @@ class LavachartsExtension extends \Twig_Extension
      */
     private $lava;
 
-    public function __construct($lava)
+    /**
+     * LavachartsExtension constructor.
+     *
+     * @param \Khill\Lavacharts\Lavacharts $lava
+     */
+    public function __construct(Lavacharts $lava)
     {
         $this->lava = $lava;
     }
 
+    /**
+     * Add Twig functions as short-cut methods to the rendering methods.
+     *
+     * @return array
+     */
     public function getFunctions()
     {
-        $chartClassesProp = new \ReflectionProperty(Lavacharts::class, 'chartClasses');
-        $chartClassesProp->setAccessible(true);
-        $chartClasses = $chartClassesProp->getValue(new Lavacharts);
+        $renderables = array_merge(['dashboard'], ChartFactory::$CHART_TYPES);
 
         $renderFunctions = [];
 
-        foreach ($chartClasses as $chartClass) {
-            $renderFunctions[] = new \Twig_SimpleFunction(strtolower($chartClass),
-                function($chartLabel, $elemId) use ($chartClass) {
-                    return $this->renderChart($chartClass, $chartLabel, $elemId);
+        foreach ($renderables as $renderable) {
+            $renderFunctions[] = new \Twig_SimpleFunction(strtolower($renderable),
+                function($label, $elementId) use ($renderable) {
+                    return $this->renderChart($renderable, $label, $elementId);
                 }
             );
         }
@@ -37,13 +53,24 @@ class LavachartsExtension extends \Twig_Extension
         return $renderFunctions;
     }
 
-    public function renderChart($chartType, $chartLabel, $elemId)
+    /**
+     * @param string $type
+     * @param string $label
+     * @param string $elementId
+     * @return string
+     */
+    public function renderChart($type, $label, $elementId)
     {
-        return $this->lava->render($chartType, $chartLabel, $elemId);
+        return $this->lava->render($type, $label, $elementId);
     }
 
+    /**
+     * Returns the name of the Lavacharts Twig Extension
+     *
+     * @return string
+     */
     public function getName()
     {
-        return 'lavacharts_twig_extension';
+        return self::NAME;
     }
 }
