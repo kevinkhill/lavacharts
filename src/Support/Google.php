96216fbc6a0e5ced455d5ff5fb95bb548eb45e12
Kevin Hill <kevinkhill@gmail.com>
2017-07-07 17:12:07 +0100

    Creating Google class to store Google related stuff

---
+++ src/Support/Google.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Khill\Lavacharts\Support;
+
+/**
+ * Google Class
+ *
+ * Simple support class for storing things google related.
+ *
+ * @package   Khill\Lavacharts\Support
+ * @since     3.2.0
+ * @author    Kevin Hill <kevinkhill@gmail.com>
+ * @copyright (c) 2017, KHill Designs
+ * @link      http://github.com/kevinkhill/lavacharts GitHub Repository Page
+ * @link      http://lavacharts.com                   Official Docs Site
+ * @license   http://opensource.org/licenses/MIT      MIT
+ */
+class Google
+{
+    const VIZ_NAMESPACE = 'google.visualization.';
+}
