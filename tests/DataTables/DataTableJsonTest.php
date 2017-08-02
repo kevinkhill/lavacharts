56ee41d58fbdea2737f8016d35b446a3dd40c68c
Kevin Hill <kevinkhill@gmail.com>
2017-08-02 18:09:12 -0400

    [ci skip] Using DataTableProvider to validate every chart types DataTable against the schema

--- tests/DataTables/DataTableJsonTest.php
+++ tests/DataTables/DataTableJsonTest.php
@@ -2,21 +2,15 @@
 
 namespace Khill\Lavacharts\Tests\Charts;
 
-use JsonSchema\Constraints\Factory;
-use JsonSchema\SchemaStorage;
-use JsonSchema\Validator;
+use Khill\Lavacharts\Charts\Chart;
+use Khill\Lavacharts\Charts\ChartFactory;
 use Khill\Lavacharts\DataTables\DataTable;
 use Khill\Lavacharts\Tests\JsonTestCase;
+use Khill\Lavacharts\Tests\Providers\DataTableProvider;
 
 class DataTableJsonTest extends JsonTestCase
 {
     const DATATABLE_SCHEMA = '../JsonSchema/datatable.json';
-
-    const COLUMN_SCHEMA = '../JsonSchema/column.json';
-
-    const ROW_SCHEMA = '../JsonSchema/row.json';
-
-    const CELL_SCHEMA = '../JsonSchema/cell.json';
 
     /**
      * @var DataTable
@@ -33,68 +27,40 @@ class DataTableJsonTest extends JsonTestCase
 
         $this->datatable->addColumns([
             ['date'  , 'Report Date'],
+            ['number', 'Profits'],
+            ['number', 'Loses'],
             ['number', 'Company Value']
         ]);
 
         for ($day = 1; $day < 30; $day++) {
             $this->datatable->addRow(
-                ['2016-1-' . $day, rand(9000000,1000000)]
+                [
+                    '2016-1-'.$day,
+                    rand(90000,100000),
+                    rand(90000,100000),
+                    rand(9000000,1000000),
+                ]
             );
         }
-
-
-        // Schema must be decoded before it can be used for validation
-        $jsonSchemaObject = json_decode(file_get_contents(self::DATATABLE_SCHEMA));
-
-        // The SchemaStorage can resolve references, loading additional schemas from file as needed, etc.
-        $schemaStorage = new SchemaStorage();
-
-        // This does two things:
-        // 1) Mutates $jsonSchemaObject to normalize the references (to file://mySchema#/definitions/integerData, etc)
-        // 2) Tells $schemaStorage that references to file://mySchema... should be resolved by looking in $jsonSchemaObject
-        $schemaStorage->addSchema('file://mySchema', $jsonSchemaObject);
-
-        // Provide $schemaStorage to the Validator so that references can be resolved during validation
-        $jsonValidator = new Validator( new Factory($schemaStorage));
-
-        // JSON must be decoded before it can be validated
-        $jsonToValidateObject = json_decode('{"data":123}');
-
-        // Do validation (use isValid() and getErrors() to check the result)
-        $jsonValidator->validate($jsonToValidateObject, $jsonSchemaObject);
     }
 
-    public function testValidateDataTableJsonAgainstSchema()
+    public function chartTypeProvider()
     {
-        $data = json_decode($this->datatable->toJson());
-
-        $this->assertValidJsonWithSchema($data, static::DATATABLE_SCHEMA);
+        return array_map(function ($chartType) {
+            return [$chartType];
+        }, ChartFactory::TYPES);
     }
 
-//    public function testValidateColumnJsonAgainstSchema()
-//    {
-//        foreach ($this->datatable->getColumns() as $column) {
-//            $this->assertValidJsonWithSchema(
-//                json_decode($column->toJson()),
-//                static::COLUMN_SCHEMA
-//            );
-//        }
-//    }
-//
-//    public function testValidateRowJsonAgainstSchema()
-//    {
-//        foreach ($this->datatable->getRows() as $row) {
-//            $this->assertValidJsonWithSchema(
-//                json_decode($row->toJson()),
-//                static::ROW_SCHEMA
-//            );
-//
-//            foreach ($row->getCells() as $cell) {
-//                $this->assertValidJsonWithSchema(
-//                    json_decode($cell->toJson()),
-//                    static::CELL_SCHEMA
-//                );
-//            }
-//        }
-//    }
+    /**
+     * @dataProvider chartTypeProvider
+     * @param string $chartType
+     */
+    public function testValidateDataTableJsonAgainstSchema($chartType)
+    {
+        $datatable = DataTableProvider::get($chartType);
+
+        $decodedDataTableJson = json_decode($datatable->toJson());
+
+        $this->assertValidJsonWithSchema($decodedDataTableJson, static::DATATABLE_SCHEMA);
+    }
 }
